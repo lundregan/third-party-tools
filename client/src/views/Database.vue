b5c9aa72e563a672f722e9a936d1ab811edd0cec
Ethan Lundregan <lundregan.ethan@gmail.com>
2021-03-01 11:53:25 +0000

    basic database display

--- client/src/views/Database.vue
+++ client/src/views/Database.vue
@@ -3,10 +3,16 @@
     <h1 class="text-blue-600 font-bold">The Database</h1>
     <h2>The Complete Database of Third-Party Tools</h2>
 
-    <div>
-      <div v-for="tool in tools" :key="tool.id">
+    <div class="mx-64">
+      <div class="flex justify-between m-2 border-b-2 border-gray-300 pb-2">
+        <p>Name</p>
+        <p>Description</p>
+        <p>Game</p>
+      </div>
+      <div v-for="tool in tools" :key="tool.id" class="flex justify-between shadow m-2">
         <p>{{tool.name}}</p>
         <p>{{tool.description}}</p>
+        <p>{{tool.game}}</p>
       </div>
     </div>
   </div>
