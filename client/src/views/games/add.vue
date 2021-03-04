071e7f0cf6faf7eee75ea12ed355023467f84b0c
Ethan Lundregan <lundregan.ethan@gmail.com>
2021-03-04 14:02:00 +0000

    added image URL to creating game entries

--- client/src/views/games/add.vue
+++ client/src/views/games/add.vue
@@ -10,8 +10,12 @@
       <label for="">Genre</label>
       <input type="text" v-model="genre" class="rounded ml-2 text-black">
     </div>
+    <div>
+      <label for="">Image URL</label>
+      <input type="text" v-model="imageURL" class="rounded ml-2 text-black">
+    </div>
 
-    <button class="bg-blue-600 p-2 rounded shadow" @click="addNewGame()">Add Game</button>
+    <button class="bg-blue-600 p-2 rounded shadow mt-8" @click="addNewGame()">Add Game</button>
   </div>
 </template>
 
@@ -22,18 +26,20 @@ export default {
   data () {
     return {
       name: '',
-      genre: ''
+      genre: '',
+      imageURL: ''
     }
   },
   methods: {
     isInputFilled: function () {
-      return this.name != '' && this.genre != '' ? true : false
+      return this.name != '' && this.genre != '' && this.imageURL != '' ? true : false
     },
     addNewGame: function () {
       if(this.isInputFilled()){
         GameService.createGame({
           name: this.name,
-          genre: this.genre
+          genre: this.genre,
+          imageURL: this.imageURL
         })
       }else{
         console.log('Inputs not filled')
