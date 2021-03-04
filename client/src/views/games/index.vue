1b1a44ff17f5ddc2c3822d11afd03254e0b9e500
Ethan Lundregan <lundregan.ethan@gmail.com>
2021-03-04 13:58:07 +0000

    better text for game icons

--- client/src/views/games/index.vue
+++ client/src/views/games/index.vue
@@ -5,8 +5,8 @@
       <div class="flex flex-wrap">
         <div v-for="game in games" :key="game.id" class="p-2 mx-8 my-2">
           <router-link :to="`games/${game._id}`">
-            <p>{{game.name}}</p>
             <img :src="game.imageURL" alt="" class="rounded-full w-64 h-64">
+            <p class="text-gray-300 text-sm mt-2">{{game.name}}</p>
           </router-link>
         </div>
       </div>
